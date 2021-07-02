f89448a489a5f8a2f2b85d736611550a8987374a
unknown <ylyangliu1@gmail.com>
2021-07-02 12:05:11 -0700

    added test

--- src/components/button/__test__/button.test.tsx
+++ src/components/button/__test__/button.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './../button';
-import { isTSAnyKeyword } from '@babel/types';
+import {render, cleanup} from '@testing-library/react';
+
+afterEach(cleanup);
 
 it("renders without crashing", () => {
     const div = document.createElement("div")
     ReactDOM.render(<Button label="Click Me"></Button>, div)
 })
+
+it("renders button correctly", () => {
+    const {getByTestId} = render(<Button label="yang"></Button>)
+    expect(getByTestId('button')).toHaveTextContent('yang')
+})
+
